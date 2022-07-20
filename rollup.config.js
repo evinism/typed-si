e430b6eef15f15da7553e39bcdfb751f87abf8fd
Evin Sellin <evinism@gmail.com>
2022-07-19 23:23:11 -0700

    typedquantities -> siunits

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ export default {
   output: {
     file: "dist/umd/index.js",
     format: "umd",
-    name: "typedquantities",
+    name: "siunits",
   },
   plugins: [nodeResolve(), commonjs(), terser(), analyze()],
 };
